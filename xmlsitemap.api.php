ef8f4711a4d0f003f08d36fe099ebb79f50fdbb9
Dave Reid <dave@davereid.net>
2009-12-23 22:29:16 +0000

    by Dave Reid: Drupal 7 port fixing round 6.

--- xmlsitemap.api.php
+++ xmlsitemap.api.php
@@ -88,23 +88,5 @@ function hook_xmlsitemap_link_alter(&$link) {
 }
 
 /**
- * Alter the query selecting data from {xmlsitemap} during sitemap generation.
- *
- * Do not alter LIMIT or OFFSET as the query will be passed through
- * db_query_range() with a set limit and offset.
- *
- * @param $query
- *   An array of a query object, keyed by SQL keyword (SELECT, FROM, WHERE, etc).
- * @param $args
- *   An array of arguments to be passed to db_query() with $query.
- * @param $language
- *   The language object being used for sitemap generation.
- */
-function hook_xmlsitemap_query_alter(array &$query, array &$args, stdClass $language) {
-  $query['WHERE'] .= " AND x.language = '%s'";
-  $args[] = $language->language;
-}
-
-/**
  * @} End of "addtogroup hooks".
  */
